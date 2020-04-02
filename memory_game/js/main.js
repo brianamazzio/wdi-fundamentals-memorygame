ed4f836a1f45d73ea4068a2fb40bf2f0f8617f4c
Briana Mazzio <62735275+brianamazzio@users.noreply.github.com>
2020-04-01 21:30:03 -0500

    Added game logic using objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,28 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+
+{
+	rank: "king",
+	suit: "queen-of-diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -16,9 +40,10 @@ function checkForMatch() {
 }
 
 function flipcard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped "  + cards[cardId].rank +  " of "  + cards[cardId].suit +  " " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 }
 
-flipcard(2);
+flipcard(0);
+flipcard(1);
